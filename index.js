724b21b75f6585b29384244f0ba7e2123d992dfc
AndroidOL <wth88888888@gmail.com>
2020-05-17 17:32:58 +0800

    HTTPS 执行

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
+const http = require("http");
+const https = require('https');
+const fs = require('fs');
+const enforceHTTPS = require('koa-sslify');
+
+// 自定义函数
+function existsFile(filename) {
+    return new Promise(
+        (complete, fail) => 
+            fs.access(filename, fs.R_OK | fs.W_OK, (err) => 
+                err ? fail(err) : complete()))
+}
+
+// Koa 运行
 const Koa = require('koa');
 const app = new Koa();
 
+const options = {
+    key: fs.readFileSync('..\\SSL\\localhost_key.pem'),
+    cert: fs.readFileSync('..\\SSL\\localhost_crt.pem')
+}
+
 app.use(async ctx => {
-  ctx.body = 'Hello World';
+    ctx.body = 'Hello World\n';
 });
 
-app.listen(3000);
+// http.createServer(app.callback()).listen(80, (err) => {
+//     if (err) {
+//         console.log('HTTP 服务启动出错', err);
+//     } else {
+//         console.log('HTTP 服务建立成功 [' + 80 + ']');
+//     }
+// });
+https.createServer(options, app.callback()).listen(443, (err) => {
+    if (err) {
+        console.log('HTTPS 服务启动出错', err);
+    } else {
+        console.log('HTTPS 服务建立成功 [' + 443 + ']');
+    }
+});
